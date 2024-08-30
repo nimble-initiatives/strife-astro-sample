e400447d24e2d370c7df338575c7feab82ff49a0
Marcus Lindblom <m@marcuslindblom.com>
2024-08-30 11:18:28 +0200

    Remove SSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import lit from "@astrojs/lit";
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,10 +21,6 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx(), lit()],
   output: "server",
   vite: {
-    plugins:[basicSsl()],
-    server: {
-      https: true
-    },
     ssr: {
       noExternal: ['@strifeapp/strife']
     }
